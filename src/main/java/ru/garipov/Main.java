e63b79ab19b0781265dad3ea4cfa2d3fa0c72649
Timur Garipov <tr.garipov@yandex.ru>
2023-02-25 20:41:42 +0300

    use User.class

--- src/main/java/ru/garipov/Main.java
+++ src/main/java/ru/garipov/Main.java
@@ -25,28 +25,30 @@ public class Main {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         httpHeaders.add("Cookie", finalSessionID);
 
-        Map<String, String> jsonData = new HashMap<>();
-//        User user = new User();
-//        user.setId(3L);
-//        user.setName("James");
-//        user.setLastName("Brown");
-//        user.setAge((byte)35);
+       // Map<String, String> jsonData = new HashMap<>();
+        User user = new User();
+        user.setId(3L);
+        user.setName("James");
+        user.setLastName("Brown");
+        user.setAge((byte)35);
 
-        jsonData.put("id", "3");
-        jsonData.put("name", "James");
-        jsonData.put("lastName", "Brown");
-        jsonData.put("age", "35");
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonData, httpHeaders);
+//        jsonData.put("id", "3");
+//        jsonData.put("name", "James");
+//        jsonData.put("lastName", "Brown");
+//        jsonData.put("age", "35");
+        HttpEntity<User> request = new HttpEntity<>(user, httpHeaders);
         sb.append(restTemplate.postForObject(url, request, String.class));
 
 
 
-        Map<String, String> jsonData2 = new HashMap<>();
-        jsonData2.put("id", "3");
-        jsonData2.put("name", "Thomas");
-        jsonData2.put("lastName", "Shelby");
-        jsonData2.put("age", "35");
-        HttpEntity<Map<String,String>> request2 = new HttpEntity<>(jsonData2, httpHeaders);
+        user.setName("James");
+        user.setLastName("Brown");
+//        Map<String, String> jsonData2 = new HashMap<>();
+//        jsonData2.put("id", "3");
+//        jsonData2.put("name", "Thomas");
+//        jsonData2.put("lastName", "Shelby");
+//        jsonData2.put("age", "35");
+        HttpEntity<User> request2 = new HttpEntity<>(user, httpHeaders);
         ResponseEntity<String> response2 = restTemplate.exchange(url,HttpMethod.PUT, request2, String.class);
         sb.append(response2.toString().split(",")[1]);
 
